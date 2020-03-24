866fe7871be9d077f2290f5d086d4ea90fde0820
sathapornma <sathaporn.ma@rmuti.ac.th>
2020-03-24 22:40:48 +0700

    update

--- Number.py
+++ Number.py
@@ -15,3 +15,18 @@ print("\n")
 #Calculate with parentheses.
 print("50 - 5*6 / 4 = ",50 - 5*6 / 4)
 print("(50 - 5*6) / 4 = ",(50 - 5*6) / 4) 
+
+print("----------------------")
+print("\n")
+
+#Using variable
+## No.1
+width = 50
+height = 10 - 5
+print("width * height = ",width * height)
+
+## No.2
+tax = 7 / 100
+price = 21
+print("price = ",round(price * tax,2))
+print("price = ",price * tax
