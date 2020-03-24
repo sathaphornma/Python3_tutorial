26b6623d84a1281d25ebff39433c38ceb43924ac
sathapornma <sathaporn.ma@rmuti.ac.th>
2020-03-24 22:22:26 +0700

    Number

---
+++ Number.py
@@ -0,0 +1,17 @@
+#Basic Operator
+print("2+2 = ",2+2)
+print("2-2 = ",2-2)
+print("2*2 = ",2*2)
+print("2**2 = ",2**2) # to the power of 2
+print("2/2 = ",2/2) # division always returns a floating point number
+print("2//2 = ",2//2) # floor division discards the fractional part
+print("2%2 = ",2%2) # the % operator returns the remainder of the division
+
+
+
+print("----------------------")
+print("\n")
+
+#Calculate with parentheses.
+print("50 - 5*6 / 4 = ",50 - 5*6 / 4)
+print("(50 - 5*6) / 4 = ",(50 - 5*6) / 4) 
